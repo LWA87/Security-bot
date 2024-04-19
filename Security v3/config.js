2bd8af2dbcaf23acfff85d64c32fc1d97c065b12
LWA87 <167525636+LWA87@users.noreply.github.com>
2024-04-19 13:24:00 +0300

    Add files via upload

---
+++ Security v3/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: '',//توكن البوت
+    status: 'discord.gg/hu',//حالة البوت
+    owners: [''],//ايديات الي يمديهم يتحكمون بالحماية
+    //Made By LWA (540056428845727746) For only Aria Host Discord (https://discord.gg/hu)
+}
